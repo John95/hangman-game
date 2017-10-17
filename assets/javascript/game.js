be97f86bc7c7e146069670d76ffdc1e2a860e0fe
unknown <jmcarroll95@gmail.com>
2017-10-17 12:53:54 -0400

    added comments and removed unneccessary code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,7 +18,7 @@ var Game = {
 		"bells",
 		"billions",
 		"adult",
-		"coders-"
+		"coders"
 	],
 	// Takes any keypress and stores it in var letter.
 	guess: function () {
@@ -76,6 +76,7 @@ var Game = {
 		// if guess not in word, returns original array, if in word, returns new array.
 		return blankArray;
 	},
+	// Takes a guess and an array and checks if guess is in array.
 	isGuessed: function (wrongArray, theGuess){
 		var letterInArray = false;
 
@@ -89,33 +90,7 @@ var Game = {
 
 }
 
-// This is the hangman word.
-// function start() {
-// 	var theList = Game.wordList;
-// 	var wordToGuess = Game.chooseWord(theList);
-
-// 	// Initialize a blank word to display on page load.
-// 	var theBlankArray = Game.makeBlankArray(wordToGuess);
-// 	var noDisplayBlankWord = Game.makeBlankWord(theBlankArray);
-// 	var displayBlankWord = Game.displayGameWord(theBlankArray);
-
-// 	// Initialize a place to put wrong guesses.
-// 	var wrongGuesses = [];
-// 	var wrongGuessCount = 0;
-
-// 	var winsCounter = 0;
-// 	var lossesCounter = 0;
-
-// 	console.log(displayBlankWord);
-// 	// When the page loads, it displays the word to be filled.
-// 	window.onload = function(event){
-// 		document.querySelector('#wordToFill').innerHTML = displayBlankWord;
-// 	}
-
-// }
-
-//Take all dat out of the function. NEXT TIME
-
+// Chooses word from a list of words.
 var theList = Game.wordList;
 var wordToGuess = Game.chooseWord(theList);
 
@@ -130,6 +105,8 @@ var wrongGuessCount = 0;
 
 var winsCounter = 0;
 var lossesCounter = 0;
+
+var initialGuesses = 9;
 
 console.log(displayBlankWord);
 
@@ -151,6 +128,8 @@ function reset() {
 // When the page loads, it displays the word to be filled.
 window.onload = function(event){
 	document.querySelector('#wordToFill').innerHTML = displayBlankWord;
+	document.querySelector('#triesLeft').innerHTML = initialGuesses;
+
 }
 
 // every key press, this runs.
@@ -175,25 +154,26 @@ document.onkeyup = function(event){
 		var updatedWord = Game.displayGameWord(updatedArray)
 		console.log(updatedWord);
 
-		// if you've already guessed this letter, skip code that increments counters.
-		// var alreadyGuessed = Game.checkWord(updatedWord, keyPress);
-
 		// GAME DISPLAY
 
+		// Shows the word that you are filling.
 		document.querySelector("#wordToFill").innerHTML = updatedWord;
+		
+		// If your guess is incorrect, pushes to wrongGuesses array and increments your misses counter.
 		if (!guessInWord) {
-			//if (Game.checkWord())
 			wrongGuesses.push(keyPress);
 			var something = Game.displayGameWord(wrongGuesses);
 			document.querySelector("#wrongGuess").innerHTML = wrongGuesses;
 			wrongGuessCount++;
 		}
 
-		var guessesLeft = (9 - wrongGuessCount);	
+		// Shows how many guesses are left.
+		var guessesLeft = initialGuesses - wrongGuessCount;
 		console.log(guessesLeft);
 		document.querySelector("#triesLeft").innerHTML = guessesLeft;
 		var compareUpdatedWord = updatedWord.replace(/\s/g, ''); // removes spaces from word.
 
+		// Checks to see if win or if you lose.
 		if (guessesLeft > 0) {
 			if(compareUpdatedWord === wordToGuess){
 				winsCounter++;
@@ -210,12 +190,8 @@ document.onkeyup = function(event){
 
 		}
 	}
-	console.log(wordToGuess);
-	//console.log(updatedWord);
-}
-// var keyPress = Game.guess();
-// console.log(keyPress);
-//console.log(Game.test);
 
-//var wordToGuess = Game.chooseWord(wordList);
-//Game.checkWord(wordToGuess, keyPress);
+	// CHEAT CODE - Uncomment to reveal what word you are guessing.
+	console.log(wordToGuess);
+}
+
